300e13425df17394a32da4885e3be5aed0abc5c5
Dipika <dipika.singhal@kelltontech.com>
2022-09-29 22:38:11 +0530

    ++ UI changes implemented

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var heading:TextView
+    lateinit var recycler:RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        heading = findViewById(R.id.heading)
+        recycler =findViewById(R.id.list)
     }
+
 }
