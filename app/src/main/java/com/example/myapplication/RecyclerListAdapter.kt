28c0e12aa6543ff4f1f07ded9951d6fb6d8aecab
Dipika <dipika.singhal@kelltontech.com>
2022-09-30 16:26:58 +0530

    ++ Added comments

--- app/src/main/java/com/example/myapplication/RecyclerListAdapter.kt
+++ app/src/main/java/com/example/myapplication/RecyclerListAdapter.kt
@@ -8,7 +8,6 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.squareup.picasso.Picasso
 
 class RecyclerListAdapter(private val context: Context, private val mList: List<ListModel>?) :
     RecyclerView.Adapter<RecyclerListAdapter.ImageListViewHolder>() {
